941fc30f53ddad3d59730328cbbae699ac3c8a1d
sunil-mnair <50510199+penroselearning@users.noreply.github.com>
2020-12-17 13:33:09 +0400

    Simple Flask Web App

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask,render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    today = datetime.now().strftime('%d-%b-%Y')
+
+    continents = ['Asia','Africa','North America','South America',
+    'Australia','Europe','Antartica']
+
+    country_capital = {'India' : 'New Delhi',
+    'USA' : 'Washington',
+    'UAE' : 'Abu Dhabi'}
+
+    return render_template("index.html",
+    today = today,
+    continents = continents,
+    country_capital = country_capital)
 
 if __name__ == "__main__":
     app.run(debug=True)
